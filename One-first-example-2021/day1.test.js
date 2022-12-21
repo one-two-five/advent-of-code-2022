9dfc7b95fe17eda193337bd3aab4825f033af7c6
Mark Waters <markman2000@googlemail.com>
2022-12-21 17:51:40 +0000

    configure jest with linter

--- One-first-example-2021/day1.test.js
+++ One-first-example-2021/day1.test.js
@@ -1,13 +1,12 @@
-const day1 = require('./day1')
+const day1 = require("./day1");
 
-describe('day1', () => {
-  test('example', () => {
-    expect(day1('./One-first/example.txt')).toBe(7)
-  })
+describe("day1", () => {
+  test("example", () => {
+    expect(day1("./One-first/example.txt")).toBe(7);
+  });
 
-  test('input', () => {
-    const output = day1('./One-first/input.txt')
-    expect(output).toBe(1752)
-  })
+  test("input", () => {
+    const output = day1("./One-first/input.txt");
+    expect(output).toBe(1752);
+  });
 });
-
