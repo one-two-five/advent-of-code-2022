7c1f22bde3be5c5cc5fa4cccdf84256722601138
Mark Waters <mark.waters@johnlewis.co.uk>
2022-12-23 15:26:56 +0000

    day 2 pt 2

--- day2/day2.2.js
+++ day2/day2.2.js
@@ -1,12 +1,71 @@
-const day2 = (input) => {
-  // const inputArr = input.split("\n\n").map((el) => el.split("\n"));
-//   const sumOfElf = inputArr.map((elf) =>
-//   elf.reduce((acc, cur) => {
-//     acc += parseInt(cur);
-//     return acc;
-//   }, 0)
-//   );
-//  return Math.max(...sumOfElf)
+// col1 = other elf
+// A = Rock, B = Paper, C = Scissors
+
+// col2 = your play
+// X = Rock, Y = Paper, Z = Scissors
+
+// Scores
+// Rock = 1, Paper = 2, Scissors = 3
+// Lose = 0, Draw = 3, Win = 6
+// Total Score === shape chosen + round outcome
+
+// Pt2
+// Second column - X = lose, Y = draw, Z = win
+
+const getShape = (elfChoice, matchOutcome) => {
+  // lose
+  if(matchOutcome === 'X') {
+    if(elfChoice == 'A') return 'Z'
+    if(elfChoice == 'B') return 'X'
+    if(elfChoice == 'C') return 'Y'
+  }
+  // draw
+  if(matchOutcome === 'Y') {
+    if(elfChoice == 'A') return 'X'
+    if(elfChoice == 'B') return 'Y'
+    if(elfChoice == 'C') return 'Z'
+  }
+  // win
+  if(matchOutcome === 'Z') {
+    if(elfChoice == 'A') return 'Y'
+    if(elfChoice == 'B') return 'Z'
+    if(elfChoice == 'C') return 'X'
+  }
+}
+
+const calculateScore = (elfChoice, myChoice) => {
+  let score = 0
+    if (myChoice === "X") {
+      score+=1
+      if (elfChoice === "B") score+=0;
+      if (elfChoice === "A") score+=3;
+      if (elfChoice === "C") score+=6;
+    }
+    if (myChoice === "Y") {
+      score+=2
+      if (elfChoice === "C") score+=0;
+      if (elfChoice === "B") score+=3;
+      if (elfChoice === "A") score+=6;
+    }
+    if (myChoice === "Z") {
+      score+=3
+      if (elfChoice === "A") score+=0;
+      if (elfChoice === "C") score+=3;
+      if (elfChoice === "B") score+=6;
+    }
+    return score
 };
 
-module.exports = day2;
+const day2 = (input) => {
+  const inputArr = input.split("\n").map(el => el.split(' '));
+  let score = 0;
+
+  inputArr.forEach((game) => {
+    const [elfChoice, matchOutcome] = game;
+    const myChoice = getShape(elfChoice, matchOutcome)
+    score+=calculateScore(elfChoice,myChoice)
+  });
+  return score
+};
+
+module.exports = { day2, calculateScore, getShape };
--- day2/day2.2.test.js
+++ day2/day2.2.test.js
@@ -1,14 +1,50 @@
-const day = require("./day2.2");
+const day2 = require("./day2.2");
 const readFile = require("../utils/readFile");
 
 test("should match example input", () => {
-  // const input = readFile("./day2/example.txt");
-  // const result = day2(input);
-  // expect(result).toBe(45000);
+  const input = readFile("./day2/example.txt");
+  const result = day2.day2(input);
+  expect(result).toBe(12);
 });
 
 test("should match final input", () => {
-  // const input = readFile("./day2/input.txt");
-  // const result = day2(input);
-  // expect(result).toBe(198041);
+  const input = readFile("./day2/input.txt");
+  const result = day2.day2(input);
+  expect(result).toBe(13448);
+});
+
+describe('calculateScore', () => {
+  test('should win', () => {
+    expect(day2.calculateScore('C','X')).toBe(7)
+    expect(day2.calculateScore('A','Y')).toBe(8)
+    expect(day2.calculateScore('B','Z')).toBe(9)
+  });
+  test('should draw', () => {
+    expect(day2.calculateScore('A','X')).toBe(4)
+    expect(day2.calculateScore('B','Y')).toBe(5)
+    expect(day2.calculateScore('C','Z')).toBe(6)
+  });
+  test('should lose', () => {
+    expect(day2.calculateScore('B','X')).toBe(1)
+    expect(day2.calculateScore('C','Y')).toBe(2)
+    expect(day2.calculateScore('A','Z')).toBe(3)
+  });
+});
+
+describe('getShape', () => {
+  test('should lose', () => {
+    expect(day2.getShape('A','X')).toBe('Z')
+    expect(day2.getShape('B','X')).toBe('X')
+    expect(day2.getShape('C','X')).toBe('Y')
+  });
+  test('should draw', () => {
+    expect(day2.getShape('A','Y')).toBe('X')
+    expect(day2.getShape('B','Y')).toBe('Y')
+    expect(day2.getShape('C','Y')).toBe('Z')
+  });
+  test('should win', () => {
+    expect(day2.getShape('A','Z')).toBe('Y')
+    expect(day2.getShape('B','Z')).toBe('Z')
+    expect(day2.getShape('C','Z')).toBe('X')
+  });
 });
