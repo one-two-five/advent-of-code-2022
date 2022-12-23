d84c500af6c2ae16b1d4c0ec44b27ac639a871b5
Mark Waters <markman2000@googlemail.com>
2022-12-23 15:49:16 +0000

    day 3 add helper method

--- day3/day3.1.js
+++ day3/day3.1.js
@@ -1,6 +1,14 @@
 
+const getPriority = (char) => {
+  const charaters = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+  'n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D',
+  'E','F','G','H','I','J','K','L','M','N','O','P','Q',
+  'R','S','T','U','V','W','X','Y','Z']
+  return charaters.findIndex((el => el === char)) + 1
+}
+
 const day3 = (input) => {
   return 0
 };
 
-module.exports = { day3 };
+module.exports = { day3, getPriority };
--- day3/day3.1.test.js
+++ day3/day3.1.test.js
@@ -13,3 +13,15 @@ test("should match final input", () => {
   // expect(result).toBe(13448);
 });
 
+describe('getPriority', () => {
+  test('should get correct priority', () => {
+    const charaters = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+    'n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D',
+    'E','F','G','H','I','J','K','L','M','N','O','P','Q',
+    'R','S','T','U','V','W','X','Y','Z']
+  
+    charaters.forEach((char, index) => {
+      expect(day3.getPriority(char)).toBe(index + 1)
+    })
+  });
+});
