68abc464f0e8c57dbf171c694fe9d3cc9fb346a5
Mark Waters <markman2000@googlemail.com>
2022-12-25 11:54:29 +0000

    day 5 TDD test cases

--- day5/day5.1.js
+++ day5/day5.1.js
@@ -1,8 +1,33 @@
+const processInput = (input) => {
+  const [start, instructions] = input.split('\n\n')
+  const numbers = start.split('\n').pop()
+  const letters = start.split('\n').slice(0, -1)
+  const processedLetters = letters.map(line => line.split('    ').map(line => line.split(' ')).flat()).reverse()
+  console.log('processedLetters', processedLetters)
+  const processedNumbers = numbers.split(' ').reduce((acc, cur) => {
+    if(parseInt(cur)) acc.push(parseInt(cur))
+    return acc
+  }, [])
+  const processedInstructions = instructions.split('\n').map(line => line.slice(5).split(' from ').map(ins => ins.split(' to ')))
+  return [processedLetters, processedNumbers, processedInstructions];
+}
 
+const moveCrates = (state, instruction) => {
+  return []
+}
 
-const day4 = (input) => {
-  let output = []
-  return output;
+const getTopCrates = (state) => {
+  return []
+}
+
+const day5 = (input) => {
+  const [startingCrates, numbers, instructions] = processInput(input)
+  let state = [...startingCrates]
+  instructions.forEach(instruction => {
+    state = moveCrates(state, instruction)
+  });
+
+  return getTopCrates()
 };
 
-module.exports = { day4 };
+module.exports = { day5, processInput, moveCrates, getTopCrates };
--- day5/day5.1.test.js
+++ day5/day5.1.test.js
@@ -1,14 +1,100 @@
-const { day5 }= require("./day5.1");
+const { day5, processInput, moveCrates, getTopCrates } = require("./day5.1");
 const readFile = require("../utils/readFile");
 
 test("should match example input", () => {
   const input = readFile("./day5/example.txt");
   const result = day5(input);
-  expect(result).toBe('CMZ');
+  expect(result).toBe("CMZ");
 });
 
 test("should match final input", () => {
   // const input = readFile("./day4/input.txt");
   // const result = day4(input);
   // expect(result).toBe(595);
+});
+
+describe("processInput", () => {
+  test("should parse input", () => {
+    const input = readFile("./day5/example.txt");
+    const expectedLetters = [
+      ["[Z]", "[M]", "[P]"],
+      ["[N]", "[C]", ""],
+      ["", "[D]", ""],
+    ];
+    const expectedNumbers = [1, 2, 3];
+    const expectedInstructions = [
+      [["1"], ["2", "1"]],
+      [["3"], ["1", "3"]],
+      [["2"], ["2", "1"]],
+      [["1"], ["1", "2"]],
+    ];
+
+    const [letters, numbers, instructions] = processInput(input);
+    expect(letters).toStrictEqual(expectedLetters);
+    expect(numbers).toStrictEqual(expectedNumbers);
+    expect(instructions).toStrictEqual(expectedInstructions);
+  });
+});
+
+describe("moveCrates", () => {
+  let state = [
+    ["[Z]", "[M]", "[P]"],
+    ["[N]", "[C]", ""],
+    ["", "[D]", ""],
+  ];
+
+  test("should move 1 from 2 to 1", () => {
+    const instruction = [1, [2, 1]];
+    const expectedState = [
+      ["[Z]", "[M]", "[P]"],
+      ["[N]", "[C]", ""],
+      ["[D]", "", ""],
+    ];
+    expect(moveCrates(state, instruction)).toStrictEqual(expectedState);
+  });
+
+  test("should move 3 from 1 to 3", () => {
+    const instruction = [3, [1, 3]];
+    const expectedState = [
+      ["", "[M]", "[P]"],
+      ["", "[C]", "[D]"],
+      ["", "", "[N]"],
+      ["", "", "[Z]"],
+    ];
+    expect(moveCrates(state, instruction)).toStrictEqual(expectedState);
+  });
+
+  test("should move 2 from 2 to 1", () => {
+    const instruction = [2, [2, 1]];
+    const expectedState = [
+      ["[C]", "", "[P]"],
+      ["[M]", "", "[D]"],
+      ["", "", "[N]"],
+      ["", "", "[Z]"],
+    ];
+    expect(moveCrates(state, instruction)).toStrictEqual(expectedState)
+  });
+
+  test("should move 1 from 1 to 2", () => {
+    const instruction = [1, [1, 2]];
+    const expectedState = [
+      ["[C]", "[M]", "[P]"],
+      ["", "", "[D]"],
+      ["", "", "[N]"],
+      ["", "", "[Z]"],
+    ];
+    expect(moveCrates(state, instruction)).toStrictEqual(expectedState)
+  });
+});
+
+describe('getTopCrates', () => {
+  test('should ', () => {
+    const state = [
+      ["[C]", "[M]", "[P]"],
+      ["", "", "[D]"],
+      ["", "", "[N]"],
+      ["", "", "[Z]"],
+    ];
+    expect(getTopCrates(state)).toBe('CMZ')
+  });
 });
