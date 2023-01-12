3b0ba6359bf9913b716e1d7c287ba9411ae47a33
Mark Waters <markman2000@googlemail.com>
2023-01-12 14:43:23 +0000

    start again - mis-read requirements

--- day8/day8.1.js
+++ day8/day8.1.js
@@ -1,58 +1,16 @@
-const cloneDeep = require('clone-deep');
+const cloneDeep = require("clone-deep");
 
-const countTreesCol = (treeArr, index, reverse = false) => {
-  let treeCount = 0;
-  let lastTreeHeight = 0;
-  const treeArrCopy = reverse ? cloneDeep(treeArr).reverse() : cloneDeep(treeArr)
-
-  treeArrCopy.every(row => {
-    const tree = row[index]
-    if (tree > lastTreeHeight) {
-      treeCount++;
-      lastTreeHeight = tree;
-      return true
-    } else {
-      return false;
-    }
+const fillArray = (arr) => {
+  const newArray = [];
+  arr.forEach((row) => {
+    newArray.push(Array(row.length).fill(0));
   });
-  return treeCount;
-};
-
-const countTreesRow = (treeArr, index, reverse = false) => {
-  let treeCount = 0;
-  let lastTreeHeight = 0;
-  const treeArrCopy = cloneDeep(treeArr);
-
-  const row = reverse ? treeArrCopy[index].reverse() : treeArrCopy[index];
-
-  row.every((tree) => {
-    console.log('tree', tree)
-    if (tree > lastTreeHeight) {
-      treeCount++;
-      lastTreeHeight = tree;
-      return true
-    } else {
-      return false;
-    }
-  });
-  return treeCount
+  return newArray;
 };
 
 const day8 = (input) => {
-  const inputArr = input.split("\n").map((row) => row.split(""));
-  let treeCount = 0
-
-  inputArr.forEach((_, index) => {
-    treeCount += countTreesRow(inputArr, index, false)
-    treeCount += countTreesRow(inputArr, index, true)
-  });
-
-  inputArr[0].forEach((_, index) => {
-    treeCount += countTreesCol(inputArr, index, false)
-    treeCount += countTreesCol(inputArr, index, true)
-  })
 
   return treeCount;
 };
 
-module.exports = { day8, countTreesCol, countTreesRow };
+module.exports = { day8, countTreesCol, countTreesRow, fillArray };
--- day8/day8.1.test.js
+++ day8/day8.1.test.js
@@ -1,4 +1,4 @@
-const { day8, countTreesCol, countTreesRow } = require("./day8.1");
+const { day8, countTreesCol, countTreesRow, fillArray } = require("./day8.1");
 const readFile = require("../utils/readFile");
 
 test("should match example input", () => {
@@ -14,21 +14,52 @@ test("should match final input", () => {
 });
 
 describe("countTreesRow", () => {
-  const trees = [[1,2,3,4,5,1,2,3,4,8,4,2,1]];
-  test("should count highest from left to right", () => {
+  test("should count highest", () => {
+    const trees = [[1,2,3,4,5,1,2,3,4,8,4,2,1]];
     expect(countTreesRow(trees, 0, false)).toBe(5);
-  });
-  test("should count highest from right to left", () => {
     expect(countTreesRow(trees, 0, true)).toBe(4);
+  });
+  test("should count highest when trees are same height", () => {
+    const trees = [[1,2,3,4,8,8,8,8,8,8,4,2,1]];
+    expect(countTreesRow(trees, 0, false)).toBe(5);
+    expect(countTreesRow(trees, 0, true)).toBe(4);
+  });
+  test("should only count first tree", () => {
+    const trees = [[9,2,3,4,8,8,8,8,8,8,4,2,9]];
+    expect(countTreesRow(trees, 0, false)).toBe(1);
+    expect(countTreesRow(trees, 0, true)).toBe(1);
   });
 });
 
 describe("countTreesCol", () => {
-  const trees = [[1], [3], [9], [5], [5], [3], [9], [6], [2], [1]];
-  test("should count highest from top to bottom", () => {
+  test("should count highest", () => {
+    const trees = [[1], [3], [9], [5], [5], [3], [9], [6], [2], [1]];
     expect(countTreesCol(trees, 0, false)).toBe(3);
-  });
-  test("should count highest from bottom to top", () => {
     expect(countTreesCol(trees, 0, true)).toBe(4);
+  });
+  test("should count highest when trees are same height", () => {
+    const trees = [[1], [3], [9], [9], [9], [9], [9], [6], [2], [1]];
+    expect(countTreesCol(trees, 0, false)).toBe(3);
+    expect(countTreesCol(trees, 0, true)).toBe(4);
+  });
+  test("should only count first tree", () => {
+    const trees = [[9], [3], [8], [8], [8], [8], [8], [6], [2], [9]];
+    expect(countTreesCol(trees, 0, false)).toBe(1);
+    expect(countTreesCol(trees, 0, true)).toBe(1);
+  });
+});
+
+describe('fillArray', () => {
+  test('fill array with 0s', () => {
+    const arr = [[1,2,3,4,5],
+                  [1,2,3,4,5],
+                  [1,2,3,4,5],
+                  [1,2,3,4,5]]
+    const expected = [
+                  [0,0,0,0,0],
+                  [0,0,0,0,0],
+                  [0,0,0,0,0],
+                  [0,0,0,0,0]]
+    expect(fillArray(arr)).toStrictEqual(expected)
   });
 });
