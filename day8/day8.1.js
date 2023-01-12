dca2ad22f7905588cc6a04ab7b3e23a9e084ba22
Mark Waters <markman2000@googlemail.com>
2023-01-12 15:28:41 +0000

    set edges to 1

--- day8/day8.1.js
+++ day8/day8.1.js
@@ -8,9 +8,41 @@ const fillArray = (arr) => {
   return newArray;
 };
 
-const day8 = (input) => {
-
-  return treeCount;
+const isEdge = (treeArray, rowIndex, colIndex) => {
+  if (
+    colIndex === 0 ||
+    rowIndex === 0 ||
+    colIndex === treeArray[0].length - 1 ||
+    rowIndex === treeArray.length - 1
+  )
+    return true;
+  return false;
 };
 
-module.exports = { day8, countTreesCol, countTreesRow, fillArray };
+const setTree = (treesVisible, rowIndex, colIndex) => {
+  const treesVisibleCopy = cloneDeep(treesVisible);
+  treesVisibleCopy[rowIndex][colIndex] = 1;
+  return treesVisibleCopy;
+};
+
+const day8 = (input) => {
+  const treeArray = input.split("\n").map((row) => row.split(""));
+  let treesVisible = fillArray(treeArray);
+
+  treeArray.forEach((row, rowIndex) => {
+    row.forEach((col, colIndex) => {
+      if (isEdge(treeArray, rowIndex, colIndex)) {
+        treesVisible = setTree(treesVisible, rowIndex, colIndex);
+      } else
+        [
+          //scan rows
+          //scan cols
+        ];
+    });
+  });
+
+  console.log("treesVisible", treesVisible);
+  return 0;
+};
+
+module.exports = { day8, fillArray };
--- day8/day8.1.test.js
+++ day8/day8.1.test.js
@@ -1,4 +1,4 @@
-const { day8, countTreesCol, countTreesRow, fillArray } = require("./day8.1");
+const { day8, fillArray } = require("./day8.1");
 const readFile = require("../utils/readFile");
 
 test("should match example input", () => {
@@ -11,42 +11,6 @@ test("should match final input", () => {
   const input = readFile("./day8/input.txt");
   // const result = day8(input);
   // expect(result).toBe(0);
-});
-
-describe("countTreesRow", () => {
-  test("should count highest", () => {
-    const trees = [[1,2,3,4,5,1,2,3,4,8,4,2,1]];
-    expect(countTreesRow(trees, 0, false)).toBe(5);
-    expect(countTreesRow(trees, 0, true)).toBe(4);
-  });
-  test("should count highest when trees are same height", () => {
-    const trees = [[1,2,3,4,8,8,8,8,8,8,4,2,1]];
-    expect(countTreesRow(trees, 0, false)).toBe(5);
-    expect(countTreesRow(trees, 0, true)).toBe(4);
-  });
-  test("should only count first tree", () => {
-    const trees = [[9,2,3,4,8,8,8,8,8,8,4,2,9]];
-    expect(countTreesRow(trees, 0, false)).toBe(1);
-    expect(countTreesRow(trees, 0, true)).toBe(1);
-  });
-});
-
-describe("countTreesCol", () => {
-  test("should count highest", () => {
-    const trees = [[1], [3], [9], [5], [5], [3], [9], [6], [2], [1]];
-    expect(countTreesCol(trees, 0, false)).toBe(3);
-    expect(countTreesCol(trees, 0, true)).toBe(4);
-  });
-  test("should count highest when trees are same height", () => {
-    const trees = [[1], [3], [9], [9], [9], [9], [9], [6], [2], [1]];
-    expect(countTreesCol(trees, 0, false)).toBe(3);
-    expect(countTreesCol(trees, 0, true)).toBe(4);
-  });
-  test("should only count first tree", () => {
-    const trees = [[9], [3], [8], [8], [8], [8], [8], [6], [2], [9]];
-    expect(countTreesCol(trees, 0, false)).toBe(1);
-    expect(countTreesCol(trees, 0, true)).toBe(1);
-  });
 });
 
 describe('fillArray', () => {
