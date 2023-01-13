09aed90900606e50ab0b1694caeb6cf7d2e67f78
Mark Waters <markman2000@googlemail.com>
2023-01-13 11:41:49 +0000

    day 8 pt 2

--- day8/day8.2.js
+++ day8/day8.2.js
@@ -1,4 +1,3 @@
-
 const fillArray = (arr) => {
   const newArray = [];
   arr.forEach((row) => {
@@ -7,67 +6,47 @@ const fillArray = (arr) => {
   return newArray;
 };
 
-const isEdge = (treeArray, rowIndex, colIndex) => {
-  if (
-    colIndex === 0 ||
-    rowIndex === 0 ||
-    colIndex === treeArray[0].length - 1 ||
-    rowIndex === treeArray.length - 1
-  )
-    return true;
-  return false;
-};
-
-const isHighest = (treeHeight, treeArr) => {
-  let isHighest = true;
+const countTrees = (treeHeight, treeArr) => {
+  let treeCount = 0;
+  let lastTreeFound = false;
   treeArr.forEach((tree) => {
-    if (tree >= treeHeight) isHighest = false;
+    if (!lastTreeFound) treeCount++;
+    if (tree >= treeHeight) lastTreeFound = true;
   });
-  return isHighest;
+  return treeCount;
 };
 
-const isTreeVisible = (row, splitIndex, treeHeight) => {
-  let treeIsVisible = false;
+const countVisibleTrees = (row, splitIndex, treeHeight) => {
+  const leftEdge = splitIndex === 0;
+  const rightEdge = splitIndex === row.length - 1;
 
-  const sliceLeft = row.slice(0, splitIndex);
-  const sliceRight = row.slice(splitIndex + 1);
+  const sliceLeft = leftEdge ? [] : row.slice(0, splitIndex);
+  const sliceRight = rightEdge ? [] : row.slice(splitIndex + 1);
+  const countLeft = countTrees(treeHeight, sliceLeft.reverse());
+  const countRight = countTrees(treeHeight, sliceRight);
 
-  if (isHighest(treeHeight, sliceLeft) || isHighest(treeHeight, sliceRight))
-    treeIsVisible = true;
-  return treeIsVisible;
+  return [countLeft, countRight];
 };
 
 const day8 = (input) => {
   const treeArray = input.split("\n").map((row) => row.split(""));
-  let treesVisible = fillArray(treeArray);
+  const treeVisibility = [];
 
   treeArray.forEach((row, rowIndex) => {
     row.forEach((_, colIndex) => {
-      if (isEdge(treeArray, rowIndex, colIndex)) {
-        treesVisible[rowIndex][colIndex] = 1
-      } else if (treesVisible[rowIndex][colIndex] === 0) {
-        const treeHeight = treeArray[rowIndex][colIndex];
-        const columnAsRow = [];
-        treeArray.forEach((row) => {
-          columnAsRow.push(row[colIndex]);
-        });
-        const row = treeArray[rowIndex];
-        if (
-          isTreeVisible(row, colIndex, treeHeight) ||
-          isTreeVisible(columnAsRow, rowIndex, treeHeight)
-        ) {
-          treesVisible[rowIndex][colIndex] = 1;
-        }
-      }
+      const treeHeight = treeArray[rowIndex][colIndex];
+      const columnAsRow = [];
+      treeArray.forEach((row) => {
+        columnAsRow.push(row[colIndex]);
+      });
+      const row = treeArray[rowIndex];
+      const [left, right] = countVisibleTrees(row, colIndex, treeHeight);
+      const [up, down] = countVisibleTrees(columnAsRow, rowIndex, treeHeight);
+      treeVisibility.push(up * down * left * right);
     });
   });
 
-  let visible = 0
-  treesVisible.forEach(row => {
-    const rowCount = row.reduce((acc, cur) => acc+=cur)
-    visible+=rowCount
-  })
-  return visible;
+  return treeVisibility.sort((a, b) => a - b).pop();
 };
 
-module.exports = { day8, fillArray, isTreeVisible };
+module.exports = { day8, fillArray, countVisibleTrees };
--- day8/day8.2.test.js
+++ day8/day8.2.test.js
@@ -1,16 +1,16 @@
-const { day8, fillArray, isTreeVisible} = require("./day8.2");
+const { day8, fillArray, countVisibleTrees} = require("./day8.2");
 const readFile = require("../utils/readFile");
 
 test("should match example input", () => {
   const input = readFile("./day8/example.txt");
   const result = day8(input);
-  expect(result).toBe(21);
+  expect(result).toBe(8);
 });
 
 test("should match final input", () => {
   const input = readFile("./day8/input.txt");
   const result = day8(input);
-  expect(result).toBe(1543);
+  expect(result).toBe(595080);
 });
 
 describe('fillArray', () => {
@@ -28,20 +28,35 @@ describe('fillArray', () => {
   });
 });
 
-describe('isTreeVisible', () => {
-  test('should set visible', () => {
+describe('countVisibleTrees', () => {
+  test('should count 2 left and right', () => {
     const row = [1,2,3,2,1]
-    expect(isTreeVisible(row, 2, 3)).toBe(true);
+    expect(countVisibleTrees(row, 2, 3)).toStrictEqual([2,2]);
   });
 
-  test('should not set visible - lower', () => {
+  test('should count 2 left and 1 right', () => {
+    const row = [1,2,3,4,5]
+    expect(countVisibleTrees(row, 2, 3)).toStrictEqual([2,1]);
+  });
+
+  test('should count 1 left and right when height is lower', () => {
     const row = [3,2,3,2,1]
-    expect(isTreeVisible(row, 1, 2)).toBe(false);
+    expect(countVisibleTrees(row, 1, 2)).toStrictEqual([1,1]);
   });
 
-  test('should not set visible - equal', () => {
+  test('should count 1 left and right when hight is lower when height is equal', () => {
     const row = [3,3,3,2,1]
-    expect(isTreeVisible(row, 1, 3)).toBe(false);
+    expect(countVisibleTrees(row, 1, 3)).toStrictEqual([1,1]);
+  });
+
+  test('should count 0 left and right 4 when at left edge', () => {
+    const row = [5,3,3,2,1]
+    expect(countVisibleTrees(row, 0, 5)).toStrictEqual([0,4]);
+  });
+
+  test('should count 0 left and right 4 when at left edge', () => {
+    const row = [2,3,3,2,5]
+    expect(countVisibleTrees(row, 4, 5)).toStrictEqual([4,0]);
   });
 });
 
