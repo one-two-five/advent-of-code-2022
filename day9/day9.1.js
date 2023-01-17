120add87ab44f9bf501c4020f93c6b3483c4efdf
Mark Waters <markman2000@googlemail.com>
2023-01-17 17:00:30 +0000

    day 9 pt 1 test cases

--- day9/day9.1.js
+++ day9/day9.1.js
@@ -1,8 +1,36 @@
+const moveHead = (lastPos, newPos) => {
+  const { row, col } = lastPos;
+  const [direction, moves] = newPos;
 
+  switch (direction) {
+    case "U":
+      return { row: (row + moves), col: col };
+    case "D":
+      return { row: (row - moves), col: col };
+    case "L":
+      return { row: row, col: (col - moves) };
+    case "R":
+      return { row: row, col: (col + moves) };
+  }
+};
+
+const moveTail = (lastPos, headPos) => {
+  return [];
+};
 
 const day9 = (input) => {
-  
+  const inputArr = input.split("\n").map((move) => {
+    const [first, second] = move.split(" ");
+    return [first, parseInt(second)];
+  });
+  const headCoords = [{ row: 0, col: 0 }];
+  const tailCoodrs = [{ row: 0, col: 0 }];
+  // grid can be any size so easier to store an object full of coordinates?
+  // e.g. H coodrs = {0,0} move up === {0,1}
+  // T looks in last H move and bases move off that
+  // work out what to add to the previous coordinate for H - up, down, left, right
+  // calculate where T is based on coordinate and add appropriate up, down, left right
   return 0;
 };
 
-module.exports = { day9 };
+module.exports = { day9, moveHead, moveTail };
--- day9/day9.1.test.js
+++ day9/day9.1.test.js
@@ -1,9 +1,9 @@
-const { day9 } = require("./day9.1");
+const { day9, moveHead, moveTail } = require("./day9.1");
 const readFile = require("../utils/readFile");
 
 test("should match example input", () => {
-  // const input = readFile("./day9/example.txt");
-  // const result = day9(input);
+  const input = readFile("./day9/example.txt");
+  const result = day9(input);
   // expect(result).toBe(0);
 });
 
@@ -11,4 +11,103 @@ test("should match final input", () => {
   // const input = readFile("./day9/input.txt");
   // const result = day9(input);
   // expect(result).toBe(1543);
+});
+
+describe("moveHead", () => {
+  test("head should move in specified direction", () => {
+    expect(moveHead({ row: 5, col: 5 }, ["U", 2])).toStrictEqual({
+      row: 7,
+      col: 5,
+    });
+    expect(moveHead({ row: 5, col: 5 }, ["D", 2])).toStrictEqual({
+      row: 3,
+      col: 5,
+    });
+    expect(moveHead({ row: 5, col: 5 }, ["L", 2])).toStrictEqual({
+      row: 5,
+      col: 3,
+    });
+    expect(moveHead({ row: 5, col: 5 }, ["R", 2])).toStrictEqual({
+      row: 5,
+      col: 7,
+    });
+  });
+});
+
+describe("moveTail", () => {
+  test("tail should move in specified direction ", () => {
+    // head above
+    expect(moveTail({ row: 4, col: 5 }, { row: 6, col: 5 })).toStrictEqual({
+      row: 5,
+      col: 5,
+    });
+    expect(moveTail({ row: 4, col: 5 }, { row: 5, col: 5 })).toStrictEqual({
+      row: 4,
+      col: 5,
+    });
+    expect(moveTail({ row: 4, col: 4 }, { row: 6, col: 5 })).toStrictEqual({
+      row: 5,
+      col: 5,
+    });
+    expect(moveTail({ row: 4, col: 6 }, { row: 6, col: 5 })).toStrictEqual({
+      row: 5,
+      col: 5,
+    });
+    // head below
+    expect(moveTail({ row: 4, col: 5 }, { row: 2, col: 5 })).toStrictEqual({
+      row: 3,
+      col: 5,
+    });
+    expect(moveTail({ row: 4, col: 5 }, { row: 3, col: 5 })).toStrictEqual({
+      row: 4,
+      col: 5,
+    });
+    expect(moveTail({ row: 4, col: 6 }, { row: 2, col: 5 })).toStrictEqual({
+      row: 3,
+      col: 5,
+    });
+    expect(moveTail({ row: 4, col: 4 }, { row: 2, col: 5 })).toStrictEqual({
+      row: 3,
+      col: 5,
+    });
+    // head right
+    expect(moveTail({ row: 4, col: 5 }, { row: 4, col: 7 })).toStrictEqual({
+      row: 4,
+      col: 6,
+    });
+    expect(moveTail({ row: 4, col: 6 }, { row: 4, col: 7 })).toStrictEqual({
+      row: 4,
+      col: 6,
+    });
+    expect(moveTail({ row: 3, col: 5 }, { row: 4, col: 7 })).toStrictEqual({
+      row: 4,
+      col: 6,
+    });
+    expect(moveTail({ row: 5, col: 5 }, { row: 4, col: 7 })).toStrictEqual({
+      row: 4,
+      col: 6,
+    });
+    // head left
+    expect(moveTail({ row: 4, col: 4 }, { row: 4, col: 2 })).toStrictEqual({
+      row: 4,
+      col: 3,
+    });
+    expect(moveTail({ row: 4, col: 4 }, { row: 4, col: 3 })).toStrictEqual({
+      row: 4,
+      col: 4,
+    });
+    expect(moveTail({ row: 5, col: 4 }, { row: 4, col: 2 })).toStrictEqual({
+      row: 4,
+      col: 3,
+    });
+    expect(moveTail({ row: 3, col: 4 }, { row: 4, col: 2 })).toStrictEqual({
+      row: 4,
+      col: 3,
+    });
+    // head covers tail - don't move
+    expect(moveTail({ row: 5, col: 5 }, { row: 5, col: 5 })).toStrictEqual({
+      row: 5,
+      col: 5,
+    });
+  });
 });
