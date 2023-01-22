967273d4207370a8237976381e5be81172cf3c65
Mark Waters <markman2000@googlemail.com>
2023-01-22 14:43:19 +0000

    day10 pt1

--- day10/day10.1.js
+++ day10/day10.1.js
@@ -1,16 +1,42 @@
 
-const day10 = (input) => {
-  const inputArr = input.split('\n').map(ins => ins.split(' ')).map(ins => {
-    if(ins[0] !== 'noop') {
-      return [ins[0], parseInt(ins[1])]
-    } else {
-      return ins
+const captureSignal = (cycle, signal, snapshots) => {
+  const snapshotsCopy = [...snapshots]
+  const signalPoints = [20, 60, 100, 140, 180, 220];
+  signalPoints.forEach((point) => {
+    if (point === cycle) {
+      snapshotsCopy.push(point * signal);
     }
-  })
-  console.log('inputArr', inputArr)
+  });
+  return snapshotsCopy;
+};
 
+const day10 = (input) => {
+  const inputArr = input
+    .split("\n")
+    .map((ins) => ins.split(" "))
+    .map((ins) => {
+      if (ins[0] !== "noop") {
+        return [ins[0], parseInt(ins[1])];
+      } else {
+        return ins;
+      }
+    });
 
-
-  return 0
-}
-module.exports = { day10 };
+  let cycle = 1;
+  let signal = 1;
+  let signalStrengths = [];
+  inputArr.forEach((ins) => {
+    if (ins[0] === "noop") {
+      signalStrengths = captureSignal(cycle, signal, signalStrengths);
+      cycle += 1;
+    } else {
+      signalStrengths = captureSignal(cycle, signal, signalStrengths);
+      cycle += 1;
+      signalStrengths = captureSignal(cycle, signal, signalStrengths);
+      cycle += 1;
+      signal += ins[1];
+    }
+  });
+  return signalStrengths.reduce((acc,cur) => acc+cur);
+};
+module.exports = { day10, captureSignal };
--- day10/day10.1.test.js
+++ day10/day10.1.test.js
@@ -1,14 +1,33 @@
-const { day10 } = require("./day10.1");
+const { day10, captureSignal } = require("./day10.1");
 const readFile = require("../utils/readFile");
 
 test("should match example input", () => {
   const input = readFile("./day10/example.txt");
   const result = day10(input);
-  expect(result).toBe(13);
+  expect(result).toBe(13140);
 });
 
 test("should match final input", () => {
-  // const input = readFile("./day10/input.txt");
-  // const result = day10(input);
-  // expect(result).toBe(6271);
+  const input = readFile("./day10/input.txt");
+  const result = day10(input);
+  expect(result).toBe(14760);
+});
+
+describe('captureSignal', () => {
+  test('should capture signal values', () => {
+    expect(captureSignal(20,2,[])).toStrictEqual([40])
+    expect(captureSignal(60,2,[])).toStrictEqual([120])
+    expect(captureSignal(100,2,[])).toStrictEqual([200])
+    expect(captureSignal(140,2,[])).toStrictEqual([280])
+    expect(captureSignal(180,2,[])).toStrictEqual([360])
+    expect(captureSignal(220,2,[])).toStrictEqual([440])
+  });
+  test('should not capture signal values', () => {
+    expect(captureSignal(19,2,[])).toStrictEqual([])
+    expect(captureSignal(21,2,[])).toStrictEqual([])
+    expect(captureSignal(70,2,[])).toStrictEqual([])
+    expect(captureSignal(101,2,[])).toStrictEqual([])
+    expect(captureSignal(203,2,[])).toStrictEqual([])
+    expect(captureSignal(225,2,[])).toStrictEqual([])
+  });
 });
