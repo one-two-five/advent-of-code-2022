abb514b369927265ded4352302a75199ec407008
Mark Waters <markman2000@googlemail.com>
2023-01-25 15:46:50 +0000

    day 11 handle input

--- day11/day11.1.test.js
+++ day11/day11.1.test.js
@@ -1,4 +1,4 @@
-const { day11 } = require("./day11.1");
+const { day11, transformInput } = require("./day11.1");
 const readFile = require("../utils/readFile");
 
 test("should match example input", () => {
@@ -11,4 +11,19 @@ test("should match final input", () => {
   const input = readFile("./day11/input.txt");
   // const result = day11(input);
   // expect(result).toBe(14760);
+});
+
+describe("transformInput", () => {
+  test("transforms input", () => {
+    const monkey = [
+      "Monkey 3:",
+      "  Starting items: 74",
+      "  Operation: new = old + 3",
+      "  Test: divisible by 17",
+      "    If true: throw to monkey 0",
+      "    If false: throw to monkey 1",
+    ];
+    const expected = [ 3, [ 74 ], [ '+', '3' ], 17, 0, 1 ]
+    expect(transformInput(monkey, 3)).toStrictEqual(expected)
+  });
 });
